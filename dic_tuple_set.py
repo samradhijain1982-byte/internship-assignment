84356211ce80c86823f7226155e21fdbb1430cc8
samradhi <samradhijain1982@gmail.com>
2026-05-20 15:15:47 +0530

    assignment 2

---
+++ dic_tuple_set.py
@@ -0,0 +1,34 @@
+# Dictionary Example
+
+student = {
+    "name": "Khushi",
+    "age": 20,
+    "course": "BCA"
+}
+print(student)
+
+print("Name:", student["name"])
+
+student["city"] = "Jaipur"
+
+print(student)
+
+# Tuple Example
+
+numbers = (10, 20, 30, 40)
+
+print(numbers)
+
+print(numbers[1])
+
+print(len(numbers))
+
+#set Example
+
+fruits = {"apple", "banana", "mango"}
+
+print(fruits)
+
+fruits.add("orange")
+
+print(fruits)
