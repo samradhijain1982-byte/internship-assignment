f659395bbdf553c9c0c78ccccaec6948d362c9b9
samradhijain1982-byte <samradhijain1982@gmail.com>
2026-05-27 21:52:18 +0530

    Create assignment_1.py for print and input practice
    Added a script to practice print statements and user input.

---
+++ assignment_1.py
@@ -0,0 +1,14 @@
+# Practice print statement
+
+print("Hello", "Ssam")
+print("Python", "Assignment", sep="-")
+print("Welcome", end=" ")
+print("User")
+
+
+# Taking input from user
+
+name = input("Enter your name: ")
+
+print("Hello", name)
+print("Welcome to Python!")
